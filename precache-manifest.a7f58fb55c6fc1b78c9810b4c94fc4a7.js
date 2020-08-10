87b8469415948fb9353ff1a357f2817c034b5ad5
olehhapuk <oleggapuk20@gmail.com>
2020-08-10 20:27:18 +0300

    Updates

---
+++ precache-manifest.a7f58fb55c6fc1b78c9810b4c94fc4a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7dde98d82f3efcf2771926082848ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff381ce62ad350442fc6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9de6dc3ebbd9a1e7211b",
+    "url": "/static/js/2.91672d12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91672d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff381ce62ad350442fc6",
+    "url": "/static/js/main.5068e9f4.chunk.js"
+  },
+  {
+    "revision": "d00c685eca9b5c60e1ce",
+    "url": "/static/js/runtime-main.bf24abff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
